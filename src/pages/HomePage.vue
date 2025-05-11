387da6c3f30cbd3d227f86840a4bf6ae39a96195
Mark0sDev <awsknows@outlook.com>
2025-05-11 20:38:34 +0300

    202

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -52,20 +52,6 @@ const leaderRouter = () => {
 </template>
 
 <style scoped lang="scss">
-.top-user-card.first-place[data-v-0ca09cec] {
-  height: 100%;
-}
-
-.first-place {
-  order: 2;
-  background-image: url('@/shared/assets/bg/top-user-1.png');
-
-  .user-avatar {
-    border: 3px solid #ebc945;
-  }
-}
-
-
 .top-user-card {
   width: 100%;
   height: 87px;
@@ -85,6 +71,75 @@ const leaderRouter = () => {
 
   &.first-place {
     height: 99px;
+  }
+}
+
+/* Аватар */
+.user-avatar-wrapper {
+  margin-top: -35px;
+  position: relative;
+
+  .medal {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: -11px;
+  }
+}
+
+.user-avatar {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+/* Имя */
+.user-name {
+  font-size: 12px;
+  font-weight: 400;
+}
+
+/* Счёт */
+.user-score {
+  font-size: 12px;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 0.7);
+}
+
+/* Корона для первого места */
+.crown {
+  position: absolute;
+  top: -72px;
+  width: 49px;
+  height: 42px;
+}
+
+/* Специальные стили для мест */
+.first-place {
+  order: 2;
+  background-image: url('@/shared/assets/bg/top-user-1.png');
+
+  .user-avatar {
+    border: 3px solid #ebc945;
+  }
+}
+
+.second-place {
+  order: 1;
+  background-image: url('@/shared/assets/bg/top-user-2.png');
+
+  .user-avatar {
+    border: 3px solid #748189;
+  }
+}
+
+.third-place {
+  order: 3;
+  background-image: url('@/shared/assets/bg/top-user-3.png');
+
+  .user-avatar {
+    border: 3px solid #af7a63;
   }
 }
 
