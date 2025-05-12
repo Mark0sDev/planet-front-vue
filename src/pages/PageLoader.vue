a1e2be76e5dd2e678b555b8f214e3a2e9a881d8e
Mark0sDev <awsknows@outlook.com>
2025-05-12 04:23:42 +0300

    Update For 12.05.2025-4-23_%d-%e-%f

--- src/pages/PageLoader.vue
+++ src/pages/PageLoader.vue
@@ -27,7 +27,17 @@ defineExpose({
 
 <style scoped>
 .loader-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+    width: 100vw;
+    height: 100vh;
+
     background-color: #151729;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .loader-img {
