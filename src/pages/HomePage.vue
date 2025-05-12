d5baa8799317fee045cf93e8ac4591c832ea7a2d
Mark0sDev <awsknows@outlook.com>
2025-05-12 18:45:48 +0300

    Update For 12.05.2025-18-45_%d-%e-%f

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -23,12 +23,14 @@ const tg = window.Telegram.WebApp;
 
 const initData = tg.initData;
 const user_id = tg.initDataUnsafe?.user?.id;
+const lang = tg.initDataUnsafe?.user?.lang;
 
 const getUser = async () => {
   await loaderRef.value?.withLoader(async () => {
     const res = await api.post('/users/getUser', {
       initData,
       user_id,
+      lang
     });
     tg.showAlert(res.data.login);
   });
