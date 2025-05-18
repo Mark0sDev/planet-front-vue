c2c64b1e779d062cb2d594ba0448c7935fa9ec2e
Mark0sDev <awsknows@outlook.com>
2025-05-18 16:21:03 +0300

    Update For 18.05.2025-16-21

--- src/utils/telegramUser.ts
+++ src/utils/telegramUser.ts
@@ -12,3 +12,4 @@ export const last_name = user?.last_name ?? '';
 export const username = user?.username ?? '';
 export const language_code = user?.language_code ?? 'en';
 export const photo_url = user?.photo_url ?? null;
+export const startParam = user?.start_param;
