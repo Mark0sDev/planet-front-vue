013d0c375770ce21f1ae478824402b08916d5ee4
Mark0sDev <awsknows@outlook.com>
2025-05-19 19:29:01 +0300

    Update For 19.05.2025-19-29

--- src/pages/BalancePage.vue
+++ src/pages/BalancePage.vue
@@ -58,7 +58,7 @@ const getUser = async () => {
       withdrawal_ton: +userApi.data.withdrawal_ton
     };
 
-    tg.showAlert(historyApi.data.status + '');
+    console.log(historyApi.data);
 
   });
 };
