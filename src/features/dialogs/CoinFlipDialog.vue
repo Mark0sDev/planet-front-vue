cab188bc18193da8f95df20166891637282c50a6
Mark0sDev <awsknows@outlook.com>
2025-05-23 15:43:36 +0300

    Update For 23.05.2025-15-43

--- src/features/dialogs/CoinFlipDialog.vue
+++ src/features/dialogs/CoinFlipDialog.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
 import {
-  tg,
   user_id,
 } from '@/utils/telegramUser';
 
-import { defineProps, reactive, ref, defineEmits } from 'vue'
+import { defineProps, defineEmits } from 'vue'
 import UiButton from '@/shared/ui/UiButton.vue'
 import UiDialog from '@/shared/ui/UiDialog.vue'
-import ConnectWalletBanner from '@/features/ConnectWalletBanner.vue';
-import { useTonWallet } from '@/utils/useTonWallet';
-import { tonConnectUI } from '@/utils/tonconnect';
+
+
 import UiDivider from '@/shared/ui/UiDivider.vue';
 import UiInput from '@/shared/ui/UiInput.vue';
-
-import axios from 'axios';
 
 interface Props {
   modelValue: boolean
@@ -23,7 +19,6 @@ interface Props {
   status: 'win' | 'lose'
 }
 
-const { isWalletConnected } = useTonWallet();
 
 const props = defineProps<Props>()
 
@@ -35,43 +30,6 @@ function close() {
   emit('update:modelValue', false)
 }
 
-const formLoaders = reactive({
-  depositTon: false,
-});
-
-const amountDepositTon = ref('');
-
-async function depositFormTon() {
-  formLoaders.depositTon = true;
-
-  try {
-    const { data } = await axios.get(`https://www.api-dev.dev/api/getMemo?comment=${user_id}`);
-
-    if (data.comment) {
-      const tonAmount = parseFloat(amountDepositTon.value);
-      const amountNano = (tonAmount * 1e9).toString();
-
-      const transaction = {
-        validUntil: Math.floor(Date.now() / 1000) + 360,
-        messages: [
-          {
-            address: 'UQA-uKB7lRsIzdjVzYCYDOkbPKUMeRZcCgehRHhX7hOwZ5SW',
-            amount: amountNano,
-            payload: data.comment,
-          },
-        ],
-      };
-
-      await tonConnectUI.sendTransaction(transaction);
-    } else {
-      tg.showAlert("Deposit error. Please contact the administrator or make a manual transfer.");
-    }
-  } catch {
-    tg.showAlert("The deposit has been canceled.");
-  } finally {
-    formLoaders.depositTon = false;
-  }
-}
 </script>
 
 <template>
@@ -82,26 +40,6 @@ async function depositFormTon() {
 
       <div v-if="walletUp">
         <div class="wallet-connect">
-          <ConnectWalletBanner />
-
-          <div v-if="isWalletConnected">
-            <form @submit.prevent="depositFormTon">
-              <UiDivider value="WEB3 Пополнение" />
-
-              <UiInput v-model="amountDepositTon" required tip="Введите сумму TON:" class="input" type="number"
-                step="0.1" placeholder="TON" min="0.1" />
-
-              <UiButton class="button" type="submit" :disabled="formLoaders.depositTon"
-                style="margin-top: 10px; margin-bottom: 10px;">
-                <template v-if="formLoaders.depositTon">
-                  <span class="spinner" />
-                </template>
-                <template v-else>
-                  Пополнить
-                </template>
-              </UiButton>
-            </form>
-          </div>
 
           <UiDivider value="Пополнить вручную" />
 
@@ -129,13 +67,6 @@ async function depositFormTon() {
   border: 1px solid #32315f;
   @include mixins.bg-cover;
 
-  // Добавлено для скролла при переполнении
-  max-height: 80vh;
-  overflow-y: auto;
-
-  
-  position: relative;
-
   &.win {
     background-image: url('@/shared/assets/bg/level-card-bg.png');
     background-position: 70% bottom;
@@ -155,16 +86,5 @@ async function depositFormTon() {
   margin-bottom: 18px;
   font-size: 16px;
   color: white;
-}
-
-.spinner {
-  width: 18px;
-  height: 18px;
-  border: 2px solid rgba(255, 255, 255, 0.4);
-  border-top-color: #ffffff;
-  border-radius: 50%;
-  animation: spin 0.6s linear infinite;
-  display: inline-block;
-  vertical-align: middle;
 }
 </style>
