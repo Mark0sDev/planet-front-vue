57b67ad6b70b0f29477f0ab96299a4958d9c45ad
Mark0sDev <awsknows@outlook.com>
2025-05-23 17:07:18 +0300

    Update For 23.05.2025-17-07

--- src/pages/BalancePage.vue
+++ src/pages/BalancePage.vue
@@ -153,7 +153,7 @@ async function withdrawalForm() {
     } else {
       showResult.value = true
       modalText.value = "Недостаточно TON на балансе"
-      modalText2.value = "Доступно к выводу: " + user.value.balance_ton + " TON";
+      modalText2.value = "Доступно к выводу: " + user.value.balance_payments_ton + " TON";
     }
   } catch {
     tg.showAlert("Withdrawal error, please try again later.");
