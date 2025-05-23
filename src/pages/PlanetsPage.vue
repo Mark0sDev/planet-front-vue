7bd70aa0a453db6ce8b64b9f9942aaf14cb4828e
Mark0sDev <awsknows@outlook.com>
2025-05-23 10:46:23 +0300

    Update For 23.05.2025-10-46

--- src/pages/PlanetsPage.vue
+++ src/pages/PlanetsPage.vue
@@ -53,7 +53,7 @@ const getUser = async () => {
     });
 
     const data = response.data;
-    const planet1 = ref(data.palnet1);
+    const planet1 = ref(data.planet_1);
 
     tg.showAlert(planet1.value + "");
 
