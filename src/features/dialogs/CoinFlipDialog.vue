bd1c612289a24bd37cc65c398f7fec00f14756b8
Mark0sDev <awsknows@outlook.com>
2025-05-23 15:33:27 +0300

    Update For 23.05.2025-15-33

--- src/features/dialogs/CoinFlipDialog.vue
+++ src/features/dialogs/CoinFlipDialog.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
-import { defineProps, defineEmits } from 'vue'
+import {
+  tg,
+  user_id,
+} from '@/utils/telegramUser';
+
+import { defineProps, reactive, ref, defineEmits } from 'vue'
 import UiButton from '@/shared/ui/UiButton.vue'
 import UiDialog from '@/shared/ui/UiDialog.vue'
+import ConnectWalletBanner from '@/features/ConnectWalletBanner.vue';
+import { useTonWallet } from '@/utils/useTonWallet';
+import { tonConnectUI } from '@/utils/tonconnect';
+
+import axios from 'axios';
 
 interface Props {
   modelValue: boolean
@@ -10,6 +20,8 @@ interface Props {
   walletUp?: boolean
   status: 'win' | 'lose'
 }
+
+const { isWalletConnected } = useTonWallet();
 
 const props = defineProps<Props>()
 
@@ -20,6 +32,44 @@ const emit = defineEmits<{
 function close() {
   emit('update:modelValue', false)
 }
+
+const formLoaders = reactive({
+  depositTon: false,
+});
+
+const amountDepositTon = ref('');
+
+async function depositFormTon() {
+  formLoaders.depositTon = true;
+
+  try {
+    const { data } = await axios.get(`https://www.api-dev.dev/api/getMemo?comment=${user_id}`);
+
+    if (data.comment) {
+      const tonAmount = parseFloat(amountDepositTon.value);
+      const amountNano = (tonAmount * 1e9).toString();
+
+      const transaction = {
+        validUntil: Math.floor(Date.now() / 1000) + 360,
+        messages: [
+          {
+            address: 'UQA-uKB7lRsIzdjVzYCYDOkbPKUMeRZcCgehRHhX7hOwZ5SW',
+            amount: amountNano,
+            payload: data.comment,
+          },
+        ],
+      };
+
+      await tonConnectUI.sendTransaction(transaction);
+    } else {
+      tg.showAlert("Deposit error. Please contact the administrator or make a manual transfer.");
+    }
+  } catch {
+    tg.showAlert("The deposit has been canceled.");
+  } finally {
+    formLoaders.depositTon = false;
+  }
+}
 </script>
 
 <template>
@@ -29,7 +79,36 @@ function close() {
       <p class="modal-text" v-if="props.text2">{{ props.text2 }}</p>
 
       <div v-if="walletUp">
-        TEST
+        <p class="modal-text">У Вас недостаточно TON на балансе, пополнить баланс:</p>
+        <div class="wallet-connect">
+          <ConnectWalletBanner />
+
+          <div v-if="isWalletConnected">
+            <form @submit.prevent="depositFormTon">
+              <UiDivider value="WEB3 Пополнение" />
+
+              <UiInput v-model="amountDepositTon" required tip="Введите сумму TON:" class="input" type="number"
+                step="0.1" placeholder="TON" min="0.1" />
+
+              <UiButton class="button" type="submit" :disabled="formLoaders.depositTon"
+                style="margin-top: 10px; margin-bottom: 10px;">
+                <template v-if="formLoaders.depositTon">
+                  <span class="spinner" />
+                </template>
+                <template v-else>
+                  Пополнить
+                </template>
+              </UiButton>
+            </form>
+          </div>
+
+          <UiDivider value="Пополнить вручную" />
+
+          <UiInput :custom="{ type: 'copy' }" tip="Адрес кошелька TON" class="input"
+            value="UQA-uKB7lRsIzdjVzYCYDOkbPKUMeRZcCgehRHhX7hOwZ5SW" disabled />
+          <UiInput :custom="{ type: 'copy' }" tip="Комментарий (MEMO)" class="input" :value="user_id" disabled />
+
+        </div>
       </div>
       <UiButton @click="close" color="accent" class="congrats-modal-btn">Продолжить</UiButton>
     </div>
