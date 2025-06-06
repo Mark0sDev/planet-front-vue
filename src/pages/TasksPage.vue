a249f4889b5ac54adcc62e9d99a1e150501cf6c2
Mark0sDev <awsknows@outlook.com>
2025-06-06 09:27:17 +0300

    Update For 06.06.2025-9-27

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import TasksBanner from '@/features/TasksBanner.vue'
+/*import TasksBanner from '@/features/TasksBanner.vue'
 import { dailyTasks, otherTasks } from '@/shared/mock/tasks.ts'
 import TaskCard from '@/entities/TaskCard/TaskCard.vue'
-import DailyAction from '@/features/DailyAction.vue'
+import DailyAction from '@/features/DailyAction.vue'*/
 </script>
 
 <template>
   <div class="tasks-page page">
-    <TasksBanner />
+    <!-- <TasksBanner />
     <DailyAction title="Транзакция" buttonText="Пополнить" variant="yellow">
       <template #reward> Награда <span>100 $NEON</span> </template>
-    </DailyAction>
+</DailyAction>
 
-    <DailyAction title="Награда" buttonText="Получить" variant="blue">
-      <template #reward> Осталось 00:00:00 </template>
-    </DailyAction>
-    <h2 class="title-1">Ежедневные задания</h2>
-    <TaskCard v-for="task in dailyTasks" :key="task.id" :task="task" />
-    <h2 class="title-1">Список заданий</h2>
-    <TaskCard v-for="task in otherTasks" :key="task.id" :task="task" />
+<DailyAction title="Награда" buttonText="Получить" variant="blue">
+  <template #reward> Осталось 00:00:00 </template>
+</DailyAction>
+<h2 class="title-1">Ежедневные задания</h2>
+<TaskCard v-for="task in dailyTasks" :key="task.id" :task="task" />
+<h2 class="title-1">Список заданий</h2>
+<TaskCard v-for="task in otherTasks" :key="task.id" :task="task" />-->
   </div>
 </template>
 
