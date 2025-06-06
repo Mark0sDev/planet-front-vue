0c513e58959a0bfede1141a8b208e0115b7b58ed
Mark0sDev <awsknows@outlook.com>
2025-06-06 13:52:58 +0300

    Update For 06.06.2025-13-52

--- src/widgets/MainDashboard.vue
+++ src/widgets/MainDashboard.vue
@@ -8,7 +8,7 @@ import { useMusic } from '@/utils/useMusic'
 
 import ComboPlanetCard from '@/features/ComboPlanetBanner.vue'
 
-import DailyDrawBanner from '@/features/DailyDrawBanner.vue'
+//import DailyDrawBanner from '@/features/DailyDrawBanner.vue'
 import UiButton from '@/shared/ui/UiButton.vue'
 import LanguageSelect from '@/shared/ui/LanguageSelect.vue'
 
@@ -50,7 +50,7 @@ const handleButtonClick = () => {
 
     </div>
 
-    <DailyDrawBanner />
+    <!--   <DailyDrawBanner />-->
 
     <ComboPlanetCard />
   </section>
