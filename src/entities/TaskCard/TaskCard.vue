407317d54484c26a5ff77acc9684527f1807fa4e
Mark0sDev <awsknows@outlook.com>
2025-06-13 13:43:52 +0300

    Update For 13.06.2025-13-43

--- src/entities/TaskCard/TaskCard.vue
+++ src/entities/TaskCard/TaskCard.vue
@@ -15,7 +15,7 @@ const handleGoClick = () => {
     if (task.link == 'story') {
       let msg = 'https://t.me/CivilizationTon_bot/app?startapp=' + user_id;
 
-      const mediaUrl = 'https://www.planetton.app/img/story.png';
+      const mediaUrl = 'https://www.planetton.app/story.png';
 
       if (language_code === 'ru' || language_code === 'ua') {
         msg += ' 🪐 6% в сутки, получай TON уже сейчас! 💎 #CivilizationTon_bot';
