1733e6f306cda7aab8860312200ec5e10d80dac1
Mark0sDev <awsknows@outlook.com>
2025-06-13 16:45:11 +0300

    Update For 13.06.2025-16-45

--- src/entities/TaskCard/TaskCard.vue
+++ src/entities/TaskCard/TaskCard.vue
@@ -55,7 +55,7 @@ const handleCheckClick = async () => {
     const data = response.data
     if (data.status == 1) {
       if (props.task.id == 1) {
-
+        alert('test');
         localBlockTimer.value = '00:59:59' // или полученное с API значение
       }
 
