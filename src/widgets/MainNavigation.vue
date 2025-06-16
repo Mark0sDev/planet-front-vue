8e48bb99004f49ad2a35bb3eba6d9bef997c2e6b
Mark0sDev <awsknows@outlook.com>
2025-06-16 12:42:52 +0300

    Update For 16.06.2025-12-42

--- src/widgets/MainNavigation.vue
+++ src/widgets/MainNavigation.vue
@@ -10,7 +10,7 @@ import { initData, user_id } from '@/utils/telegramUser'
 const { t } = useI18n()
 const route = useRoute()
 
-const allTaskIds = ref<number[]>([2, 3, 4, 5, 6, 7, 8, 9, 10])
+const allTaskIds = ref<number[]>([2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 const tasksCount = ref(0)
 
 interface TaskFromApi {
